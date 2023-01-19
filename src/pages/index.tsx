f03fd5371e14f76daa034021f220789b182c9908
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2023-01-19 16:25:52 +0100

    * updated   - Hope it will fix 500 error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,17 @@
 import type {GetSessionParams} from "next-auth/react";
 import {getSession} from "next-auth/react";
-import {AddKanjiListToSingleUser, trpc} from "@/src/utils";
-import {Button, Group} from "@mantine/core";
-import useUser from "@/hooks/useUser";
-import {IconSchool, IconVocabulary} from "@tabler/icons";
 import LoadingOverlayCustom from "@/components/overlay/LoadingOverlay.Custom";
-import {useRouter} from "next/router";
-import NextLevelStatus from "@/components/review/NextLevelStatus";
+import useUser from "@/hooks/useUser";
+
 
 export default function Home() {
-	const {data} = trpc.userKanji_all.useQuery();
-	const {push} = useRouter();
-	const user = useUser();
+const user = useUser();
 
-	if (!user) return null;
 
-	const lessons = data?.filter(({lesson_status, kanji_level, kanjiId, kanji}) =>
-			kanjiId === `${kanji}_${user.id}`
-			&& lesson_status
-			&& kanji_level === user?.curentlevel,
-	);
-
-	const reviews = data?.filter(({lesson_status, kanji_level, kanjiId, kanji, correct_answers}) =>
-			kanjiId === `${kanji}_${user.id}`
-			&& !lesson_status
-			&& kanji_level <= user?.curentlevel!
-			&& correct_answers < 5,
-	);
 
 	return (
-			<LoadingOverlayCustom visible={user.isLoading}>
-				<Group position={"center"}>
-					<Button onClick={() => push("/lesson")} disabled={lessons?.length === 0}
-					        leftIcon={<IconSchool/>}>Lessons {lessons?.length}</Button>
-					<Button onClick={() => push("/review")} disabled={reviews?.length === 0}
-					        leftIcon={<IconVocabulary/>}>Reviews {reviews?.length}</Button>
-					<NextLevelStatus/>
-
-				</Group>
+			<LoadingOverlayCustom visible={user?.isLoading}>
+			test
 			</LoadingOverlayCustom>
 	);
 }
@@ -56,12 +30,9 @@ export async function getServerSideProps(context: GetSessionParams) {
 				permanent: false,
 			},
 		};
-	} else {
-		await AddKanjiListToSingleUser(session);
-
+	}
 		return {
 			props: {session},
 		};
-	}
 
 }
