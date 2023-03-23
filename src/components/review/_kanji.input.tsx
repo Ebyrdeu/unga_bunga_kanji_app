28d982ba8b4ca47afe16d0e2e9a91a5defec3aa0
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2023-03-23 09:50:24 +0100

    fixed bug where on level 1 it's decreased to level 0

--- src/components/review/_kanji.input.tsx
+++ src/components/review/_kanji.input.tsx
@@ -9,7 +9,7 @@ import {api} from '@utils/api';
 
 export const KanjiInput = ({kanjiData}: { kanjiData: UserKanji }) => {
   const theme = useMantineTheme();
-  const {levelUpUser} = useLevelUp()
+  const {levelUpUser} = useLevelUp();
   const utils = api.useContext();
   const {disabled, correct, toggles, reading, gameActions} = useKanjiGameStore();
 
@@ -17,8 +17,6 @@ export const KanjiInput = ({kanjiData}: { kanjiData: UserKanji }) => {
   const {mutate: newTime} = api.srs.updateSRS.useMutation({
     onSuccess: () => utils.user.getUserKanji.invalidate(),
   });
-
-
 
   // Form functions
   const onCorrectAnswer = (reading: string) => {
@@ -46,15 +44,15 @@ export const KanjiInput = ({kanjiData}: { kanjiData: UserKanji }) => {
         if (correct !== theme.colors.red[9]) return toggles.isCorrect(theme.colors.red[9]);
         toggles.isCorrect(null);
         toggles.toggleDisabled(false);
+        const checkOnLowLevel = kanjiData[0].srs_stage <= 1 ? 1 : kanjiData[0].srs_stage - 1;
 
-        const data = SRS(kanjiData[0], kanjiData[0].srs_stage < 1 ? 0 : kanjiData[0].srs_stage - 1);
+        const data = SRS(kanjiData[0], checkOnLowLevel);
 
         newTime({
           data: {
             id: kanjiData[0].kanjiId, srs_stage: data.srs_stage, updatedAt: data.updatedAt,
           },
         });
-
         gameActions.reset();
       }
     }
