0b642704bc8c448467c7b32a88d2c77cbf0f3917
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2023-03-23 09:51:50 +0100

    change form back to push

--- src/components/lesson/_noLesson.tsx
+++ src/components/lesson/_noLesson.tsx
@@ -3,14 +3,14 @@ import {IconArrowBack} from '@tabler/icons';
 import {useRouter} from 'next/router';
 
 export const NoLesson = () => {
-  const {back} = useRouter();
+  const {push} = useRouter();
   return (
       <div>
         <Text my={20} align={'center'} fz="xl" tt="uppercase" fw={700} c="dimmed">
           No lesson available
         </Text>
         <Group position={'center'}>
-          <Button onClick={() => back()} leftIcon={<IconArrowBack/>} size="md">Take me back to previous
+          <Button onClick={() => void push('/')} leftIcon={<IconArrowBack/>} size="md">Take me back to previous
             page</Button>
         </Group>
       </div>
